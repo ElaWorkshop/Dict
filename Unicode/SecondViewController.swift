4c0a772d9655f4ea518bdc31842a7e495eeb8e94
LIU Dongyuan / 柳东原 <liu.dongyuan@gmail.com>
2015-12-29 00:39:02 -0800

    Add Unicode app target

---
+++ Unicode/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Unicode
+//
+//  Created by Dongyuan Liu on 2015-12-29.
+//  Copyright © 2015 Ela Workshop. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
