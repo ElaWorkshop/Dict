1119483120dab9e7273cfdb0fb5ed549747216ef
LIU Dongyuan / 柳东原 <liu.dongyuan@gmail.com>
2015-12-29 00:05:43 -0800

    Initial commit

---
+++ Dict/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Dict
+//
+//  Created by Dongyuan Liu on 2015-12-29.
+//  Copyright © 2015 Ela Workshop. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# Dict
+
+A universal dictionary application.
